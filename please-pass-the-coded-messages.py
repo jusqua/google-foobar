355ca9aa098895dfdfdeb178ae50963ae877c046
Ádrian Gama <adrian.gama.se@gmail.com>
2023-10-31 20:04:29 +0000

    Remove dummy `ml` variable

--- please-pass-the-coded-messages.py
+++ please-pass-the-coded-messages.py
@@ -1,14 +1,14 @@
 def solution(l):
     l.sort(reverse=True)
     diff = sum(l) % 3
-    ml = 0
+
     while sum(l) % 3 != 0:
         rm = list(filter(lambda n: n % 3 == diff, l))
-        if len(rm) > ml:
-            for e in rm[-1 - ml:]:
-                l.remove(e)
+        if len(rm) != 0:
+            l.remove(rm[-1])
         diff = 1 if diff == 2 else 2
-        ml += -ml
+
     if not l:
         return 0
+
     return int(''.join(map(str, l)))
